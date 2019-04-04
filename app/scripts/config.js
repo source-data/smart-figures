b4bccec4fb24a6f3f0a51f664c9eb42f3c5e6b17
Robin Liechti <robin.liechti@isb-sib.ch>
2019-04-04 16:51:26 +0200

    put organisation json+ld in index.html

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {serverURL:'https://api.sourcedata.io/',baseURL:'/',baseHref:'/',withCredentials:true,debugInfoEnabled:true,CORS:true,httpCache:false,html5:false})
+.constant('ENV', {serverURL:'https://api.sourcedata.io/',baseURL:'https://search.sourcedata.io/',baseHref:'/',withCredentials:true,debugInfoEnabled:false,CORS:true,httpCache:true,html5:true})
 
 ;
